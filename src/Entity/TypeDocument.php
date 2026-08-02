6ab0fea35599ea50dafd3b0965a9d3ce1ca495bb
Walker-19 <walkerabraham48@gmail.com>
2026-08-02 15:31:10 +0200

    fix: ownerRequest

--- src/Entity/TypeDocument.php
+++ src/Entity/TypeDocument.php
@@ -16,11 +16,11 @@ class TypeDocument
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['document:read', 'referentiel:read', 'bateau:read'])]
+    #[Groups(['document:read', 'referentiel:read', 'bateau:read', 'owner_request:read'])]
     private ?int $id = null;
 
     #[ORM\Column(name: 'label_type_document', length: 150)]
-    #[Groups(['document:read', 'referentiel:read', 'bateau:read'])]
+    #[Groups(['document:read', 'referentiel:read', 'bateau:read', 'owner_request:read'])]
     private string $labelTypeDocument;
 
     #[ORM\OneToMany(targetEntity: Document::class, mappedBy: 'typeDocument')]
